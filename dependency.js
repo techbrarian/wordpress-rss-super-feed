6f0f8ac6e6f0999637006a5f9695974d95352d8d
Jason Bengtson, MLIS, MA <j.bengtsonunm@gmail.com>
2013-09-13 13:00:49 -0600

    Create dependency.js
    add this to the header of your page

---
+++ dependency.js
@@ -0,0 +1,24 @@
+//Bengtson-Fu!!!
+///created by Jason Bengtson, MLIS, MA
+///dependency for rss super-feed
+
+var usethis=/<.+?>/g;
+var replacethis= "></a>";
+var withthis="></a><br>";
+var isitjunk=0;
+
+function fixitfunc()
+{
+if (calledit>testoxe)
+{
+calledit==testoxe-4;
+hanger=hanger.replace("</p>","");
+}
+
+if (hanger.indexOf("<code>")>0)
+{
+hanger=hanger.replace("</code>","");
+hanger=hanger.replace("<code>","");
+calledit=calledit-13;
+}
+}
